9d8e3a3734c1ae78b98f033c0e2aa881b6cb988f
Виталий <w11b@ya.ru>
2018-05-08 02:18:35 +0300

    first

---
+++ Minifying/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AntlrGrammars;
+
+namespace Minifying
+{
+    class Class1
+    {
+        HtmlLexer htmlLexer;
+    }
+}
